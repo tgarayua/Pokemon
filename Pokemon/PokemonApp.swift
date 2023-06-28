9d590f52bf67ae6a7c60477d85bb5ab45b935111
Thomas Garayua <tgarayua@gmail.com>
2023-06-27 21:06:25 -0400

    first commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Thomas Garayua on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
